3b6de6ee53c08f5cfddf715fe78af107a06cba99
dwil098 <123797422+dwil098@users.noreply.github.com>
2023-05-22 18:50:03 +0800

    Add files via upload

---
+++ Portfolio Project/sample-file.js
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <p id= "edwil"></p>
+
+        <script>
+var e = "Hi im edwil";
+function nameFunction() {
+    return e;   
+}
+
+let edwil = nameFunction();
+document.getElementById("edwil").innerHTML = edwil;
+
+
+        </script>
+
+    </body>
+</html>
